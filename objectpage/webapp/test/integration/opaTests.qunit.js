a358ac3b10e3c6a6421718879eb86bac565fecc6
swapv74 <swapnilv0209@gmail.com>
2025-05-07 15:15:21 +0530

    Delete objectpage directory

--- objectpage/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["objectpage/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- objectpage/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"objectpage/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
